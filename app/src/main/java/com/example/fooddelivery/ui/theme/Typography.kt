322708394fc9a43ee9a8e5948dd8b64ee9f1c799
pamzak <pamirzaki2014@gmail.com>
2024-09-21 16:29:46 +0430

    add the font class

--- app/src/main/java/com/example/fooddelivery/ui/theme/Typography.kt
+++ app/src/main/java/com/example/fooddelivery/ui/theme/Typography.kt
@@ -1,7 +1,9 @@
 package com.example.fooddelivery.ui.theme
 
+import androidx.compose.material3.Text
 import androidx.compose.material3.Typography
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -19,4 +21,68 @@ Font(R.font.font_light,FontWeight.Light, FontStyle.Normal)
 )
 
 @Immutable
-data class  AppFont
+data class  AppTypography(
+    val headline:TextStyle,
+    val titleLarge:TextStyle,
+    val titleMedium:TextStyle,
+    val titleSmall:TextStyle,
+
+    val body:TextStyle,
+    val  bodySmall:TextStyle,
+    val label:TextStyle,
+
+)
+
+val LocalAppTypography= staticCompositionLocalOf {
+    AppTypography(
+        headline = TextStyle.Default,
+        titleLarge = TextStyle.Default,
+  titleMedium = TextStyle.Default,
+titleSmall=TextStyle.Default,
+body = TextStyle.Default,
+        bodySmall = TextStyle.Default,
+        label= TextStyle.Default
+
+
+    )
+
+    val extendedTypography=AppTypography(
+
+        headline = TextStyle(
+            fontFamily = InriasanFontFamily,
+            fontSize = 32.sp,
+             fontWeight = FontWeight.Normal,
+            ),
+        titleLarge = TextStyle(
+            fontFamily = InriasanFontFamily,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Normal,
+        ),
+        titleMedium = TextStyle(
+            fontFamily = InriasanFontFamily,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Normal,
+        ),
+        titleSmall = TextStyle(
+            fontFamily = InriasanFontFamily,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Normal,
+        ),
+        body = TextStyle(
+            fontFamily = InriasanFontFamily,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Normal,
+        ),
+        bodySmall = TextStyle(
+            fontFamily = InriasanFontFamily,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Normal,
+        ),
+        label = TextStyle(
+            fontFamily = InriasanFontFamily,
+            fontSize = 11.sp,
+            fontWeight = FontWeight.Light,
+        ),
+
+    )
+}
